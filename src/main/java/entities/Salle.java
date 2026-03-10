4dac489822a331df8634c0338853d61db53384fb
zakiaazizi-17 <z.azizi2447@uca.ac.ma>
2026-03-10 16:18:26 +0000

    TP4JEE

---
+++ src/main/java/entities/Salle.java
@@ -0,0 +1,53 @@
+package entities;
+
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "salles")
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+
+    @OneToMany(mappedBy = "salle", fetch = FetchType.EAGER)
+    private List<Machine> machines;
+
+    public Salle( String code, List<Machine> machines) {
+
+        this.code = code;
+        this.machines = machines;
+    }
+
+    public Salle() {
+    }
+
+    public Salle(String b2) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public List<Machine> getMachines() {
+        return machines;
+    }
+
+    public void setMachines(List<Machine> machines) {
+        this.machines = machines;
+    }
+}
